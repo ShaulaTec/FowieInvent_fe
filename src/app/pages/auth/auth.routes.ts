f3902768931c5a2638367ff39002980611882a8b
YalukR <yalukramos@gmail.com>
2026-07-06 19:07:20 -0600

    register refactorizado

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Access } from './access';
 import { Login } from './login';
 import { Error } from './error';
-import { Register } from './register';
+import { Register } from './register/register';
 
 export default [
     { path: 'access', component: Access },
