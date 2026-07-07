922bb708ebc2998b9678d797ebb7eb3cafa15b2f
YalukR <yalukramos@gmail.com>
2026-07-06 19:09:30 -0600

    chore: dejar de trackear environment.ts

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8000/api',
-    auth: {
-        tokenUrl: '/token/',
-        refreshUrl: '/token/refresh/',
-        verifyUrl: '/token/verify/',
-    }
-};
